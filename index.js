52644a9c61626e3541be0c4cdf8b0fc39e1702fa
Kathy Vira <katy2002@gmail.com>
2021-07-25 23:20:18 +0300

    keys git ignore

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const app = express();
+const passport = require('passport');
+const GoogleStratedy = require('passport-google-oauth20');
 
-app.get('/', (req, res) => {
-    res.send({ bye: 'buddy' });
-});
+// console.developers.google.com
+
+const app = express();
+passport.use(new GoogleStratedy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
